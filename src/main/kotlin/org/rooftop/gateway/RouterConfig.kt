1452325a0d2189e050ffb2f7161d558a81a4795b
xb205 <62425964+devxb@users.noreply.github.com>
2023-11-01 21:08:27 +0900

    feat: identity-server API router 구성 (#6)

--- src/main/kotlin/org/rooftop/gateway/RouterConfig.kt
+++ src/main/kotlin/org/rooftop/gateway/RouterConfig.kt
@@ -1,21 +1,53 @@
 package org.rooftop.gateway
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.cloud.gateway.route.RouteLocator
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod.GET
-import java.net.URI
+import org.springframework.http.HttpMethod.*
 
 @Configuration
-internal class RouterConfig {
-
+internal class RouterConfig(
+    @Value("\${rooftop.server.identity.host}") private val identityHost: String,
+    @Value("\${rooftop.server.identity.url}") private val identityUrl: String,
+    @Value("\${rooftop.server.identity.default-url}") private val defaultUrl: String,
+) {
     @Bean
     internal fun gateways(routeLocatorBuilder: RouteLocatorBuilder): RouteLocator {
         return routeLocatorBuilder.routes()
-            .route("sample-router") {
-                it.method(GET).and().path("/sample")
-                    .uri(URI.create("https://google.com"))
+            .route("identity#create-user") {
+                it.method(POST)
+                    .and().host(identityHost)
+                    .and().path(defaultUrl)
+                    .uri(identityUrl)
+            }
+            .route("identity#login") {
+                it.method(POST)
+                    .and().host(identityHost)
+                    .and().path("/v1/logins")
+                    .uri(identityUrl)
+            }
+            .route("identity#find-user-by-name") {
+                it.method(GET)
+                    .and().host(identityHost)
+                    .and().path(defaultUrl)
+                    .and().query("name")
+                    .uri(identityUrl)
+            }
+            .route("identity#update-user") {
+                it.method(PUT)
+                    .and().host(identityHost)
+                    .and().path(defaultUrl)
+                    .filters { it }
+                    .uri(identityUrl)
+            }
+            .route("identity#delete-user") {
+                it.method(DELETE)
+                    .and().host(identityHost)
+                    .and().path(defaultUrl)
+                    .filters { it }
+                    .uri(identityUrl)
             }
             .build()
     }
---
+++ src/test/kotlin/org/rooftop/gateway/GatewayTest.kt
@@ -0,0 +1,98 @@
+package org.rooftop.gateway
+
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.datatest.withData
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpMethod.*
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.web.util.DefaultUriBuilderFactory
+
+@AutoConfigureWireMock(port = 8081)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+internal class GatewayTest(
+    private val wireMockServer: WireMockServer,
+    @LocalServerPort private val port: Int,
+) : FunSpec({
+
+    val webClient = WebTestClient.bindToServer()
+        .baseUrl("http://localhost:$port").build();
+
+    beforeSpec {
+        wireMockServer.stubFor(
+            WireMock.any(WireMock.anyUrl())
+                .willReturn(
+                    WireMock.aResponse().withStatus(ROUTE_SUCCEED_STATUS)
+                        .withHeader(routeSucceedHeader.first, routeSucceedHeader.second)
+                )
+        )
+    }
+
+    context("Routing 가능한 URL이 주어지면, Routing을 성공한다 반환한다.") {
+        withData(nameFn = { it.description }, apis) {
+            val client =
+                webClient.method(it.method)
+                    .uri(
+                        DefaultUriBuilderFactory().builder()
+                            .scheme("http")
+                            .host("localhost")
+                            .path(it.path)
+                            .port(port)
+                            .query(it.param)
+                            .build()
+                    )
+
+            it.headers.forEach { (header, value) -> client.header(header, value) }
+
+            client.exchange()
+                .expectStatus().isEqualTo(ROUTE_SUCCEED_STATUS)
+                .expectHeader().valueEquals(routeSucceedHeader.first, routeSucceedHeader.second)
+        }
+    }
+}) {
+    companion object TestSuite {
+        private val routeSucceedHeader = "Routed" to "succeed"
+        private const val ROUTE_SUCCEED_STATUS = 404
+        private const val IDENTITY_SERVER_HOST = "localhost"
+
+        private val apis: List<Api> = listOf(
+            Api(POST, IDENTITY_SERVER_HOST, "/v1/users", description = "create user"),
+            Api(POST, IDENTITY_SERVER_HOST, "/v1/logins", description = "login"),
+            Api(
+                GET,
+                IDENTITY_SERVER_HOST,
+                "/v1/users",
+                param = "name=hello_msa",
+                description = "get user by name"
+            ),
+            Api(
+                PUT,
+                IDENTITY_SERVER_HOST,
+                "/v1/users",
+                headers = mapOf(HttpHeaders.AUTHORIZATION to "jwt.jwt.jwt"),
+                description = "update user"
+            ),
+            Api(
+                DELETE,
+                IDENTITY_SERVER_HOST,
+                "/v1/users",
+                headers = mapOf(HttpHeaders.AUTHORIZATION to "jwt.jwt.jwt"),
+                description = "delete user"
+            ),
+        )
+    }
+
+    private data class Api(
+        val method: HttpMethod,
+        val host: String,
+        val path: String,
+        val param: String = "",
+        val headers: Map<String, String> = mapOf(),
+        val description: String = "",
+    )
+}
